dd7ecaaa9a1108f7dbe929426838554588a70a40
janny24 <jannysankar@gmail.com>
2021-07-05 20:34:11 +0530

    variable hoisting and function hoisting

--- js_series1.js
+++ js_series1.js
@@ -1,7 +1,19 @@
-// Hoisting in js
+//variable Hoisting in js
 
 function test(){
     console.log(x) //the result will be undifined
-}test() //function called befor decleration
+}test() //function called befor decleration  ==> this kind of mechanism is called hoisting
 
 var x = 15;
+
+// Function hoisting in js
+function test1(){           //this is called named function
+    console.log("hello")
+}
+
+var a=function(){           //this is called anonyms function which means nameless function  
+    console.log("hi")
+}
+
+test();
+a();
